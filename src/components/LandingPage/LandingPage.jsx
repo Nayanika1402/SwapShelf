45ec6d35c2b5943804cf2ec6cbcc996aa2c0c1e8
Nayanika1402 <132455412+Nayanika1402@users.noreply.github.com>
2024-08-22 23:16:54 +0530

    NavBar fixed

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import '../../index.css';
-import { BiSun, BiMoon } from 'react-icons/bi';
-import KnowledgeAnimate from '../../images/knowledge-animate[1].svg'; // Update with the correct path
+import Navbar from '../Navbar'; 
+import KnowledgeAnimate from '../../images/knowledge-animate[1].svg'; 
 
 const LandingPage = () => {
-  const [lightMode, setLightMode] = useState(true); // Default to light mode
+  const [lightMode, setLightMode] = useState(true); 
 
   const toggleLightMode = () => {
     setLightMode(!lightMode);
@@ -12,22 +12,12 @@ const LandingPage = () => {
 
   return (
     <div className={lightMode ? 'light-mode' : 'dark-mode'}>
-      <nav className="navbar">
-        <div className="logo">
-          <h1>SwapShelf</h1>
-        </div>
-        <div className="nav-links">
-          <button className="login-btn">Login</button>
-          <button onClick={toggleLightMode} className="toggle-mode-btn">
-            {lightMode ? <BiSun size={24} /> : <BiMoon size={24} />}
-          </button>
-        </div>
-      </nav>
+      <Navbar lightMode={lightMode} toggleLightMode={toggleLightMode} />
       <header className="main-section">
         <div className="main-content">
           <h1>Connect, Swap, and Explore Your Next Great Read!</h1>
           <p>
-          Join a thriving community to exchange books and discover new literary adventures.
+            Join a thriving community to exchange books and discover new literary adventures.
           </p>
           <button className="explore-btn">Explore More</button>
         </div>

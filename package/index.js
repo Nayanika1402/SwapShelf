bc27ce7191ac8dac7d941953f8e7c897ee1458c2
Pujan-sarkar <pujansarkar37@gmail.com>
2024-08-23 09:34:51 +0530

    feat:Connection fix

--- package/index.js
+++ package/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://swap-shelf.vercel.app', 
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
 }));
